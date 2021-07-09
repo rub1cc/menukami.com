fa344bee14176a62ebb400d94d63a04879339ce6
farazaulia <raz.muhammad14@gmail.com>
2021-07-09 08:33:12 +0700

    update ui

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -2,6 +2,7 @@ import DesktopLayout from 'layouts/DesktopLayout'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
+import Logo from './Logo'
 
 function LoginButton() {
   return (
@@ -23,19 +24,7 @@ function HomeHeader() {
       <DesktopLayout>
         <div className="flex justify-between py-5">
           <button className="text-2xl flex items-center space-x-3" onClick={() => router.push('/')}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-8 h-8 text-white bg-blue-500 rounded-full p-1"
-            >
-              <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-              <path
-                fillRule="evenodd"
-                d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <Logo />
             <span className="tracking-tighter">menukami</span>
           </button>
           <LoginButton />
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,17 +1,3 @@
 export default function Logo() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-      className="w-8 h-8 text-white bg-blue-500 rounded-full p-1"
-    >
-      <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-      <path
-        fillRule="evenodd"
-        d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-        clipRule="evenodd"
-      />
-    </svg>
-  )
+  return <img src="/images/logo.png" className="w-8 h-8" alt="Menukami logo" />
 }
