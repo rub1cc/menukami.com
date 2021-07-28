4e236a7b78b4e3c18357ae75a25cebaff7c80f8f
farazaulia <raz.muhammad14@gmail.com>
2021-07-28 12:25:10 +0700

    fix: sitemap not found

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,77 +1,32 @@
-import fs from 'fs'
+// pages/server-sitemap.xml/index.tsx
+
+import { getServerSideSitemap } from 'next-sitemap'
 import { supabase } from 'utils/supabaseClient'
 
-const Sitemap = () => {}
-
-export const getServerSideProps = async ({ res }) => {
+export const getServerSideProps = async (ctx) => {
   const baseUrl = {
     development: 'http://localhost:3000',
     production: 'https://menukami.com',
   }[process.env.NODE_ENV]
-
-  const staticPages = fs
-    .readdirSync('pages')
-    .filter((staticPage) => {
-      return ![
-        '_app.js',
-        '_document.js',
-        '_error.js',
-        '[outlet_slug]',
-        'admin',
-        'api',
-        'privacy.js',
-        'index.js',
-        'sitemap',
-      ].includes(staticPage)
-    })
-    .map((staticPagePath) => {
-      return `${baseUrl}/${staticPagePath}`
-    })
-
   const { data } = await supabase.from('outlets').select('*')
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>${baseUrl}/</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>1.0</priority>
-    </url>
-      ${staticPages
-        .map((url) => {
-          return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `
-        })
-        .join('')}
-      ${data
-        .map(({ slug, updated_at }) => {
-          return `
-              <url>
-                <loc>${baseUrl}/${slug}</loc>
-                <lastmod>${updated_at}</lastmod>
-                <changefreq>weekly</changefreq>
-                <priority>0.9</priority>
-              </url>
-            `
-        })
-        .join('')}
-    </urlset>
-  `
+  const fields = [
+    {
+      loc: baseUrl, // Absolute url
+      lastmod: new Date().toISOString(),
+      changefreq: 'weekly',
+      priority: '1.0',
+    },
+    ...data.map(({ slug, updated_at }) => ({
+      loc: `${baseUrl}/${slug}`, // Absolute url
+      lastmod: updated_at,
+      changefreq: 'weekly',
+      priority: '0.9',
+    })),
+  ]
 
-  res.setHeader('Content-Type', 'text/xml')
-  res.write(sitemap)
-  res.end()
-
-  return {
-    props: {},
-  }
+  return getServerSideSitemap(ctx, fields)
 }
 
-export default Sitemap
+// Default export to prevent next.js errors
+export default () => {}
